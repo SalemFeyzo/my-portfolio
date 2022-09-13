f55de153692ec2fb4aba6ba6cf0fdd4be86fc4fe
salemFeyzo <f1989salim@gmail.com>
2022-09-13 16:03:57 +0300

    init

---
+++ src/lib/components/ThemeSwitch.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Dialog, { Title, Content, Actions } from '@smui/dialog';
+	import Button, { Label } from '@smui/button';
+	import Select, { Option } from '@smui/select';
+	import { theme } from '$lib/stores';
+
+	let open = false;
+	let th = ['system', 'light', 'dark'];
+</script>
+
+<Select bind:value={$theme} label="Select Theme">
+	<Option value="system">System</Option>
+	<Option value="light">Light</Option>
+	<Option value="dark">Dark</Option>
+</Select>
