cec835ee8a3dde084f02e3ff08629b04eb98ea20
salemFeyzo <f1989salim@gmail.com>
2022-09-17 14:10:14 +0300

    Nav Drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,28 @@
-import * as React from "react";
+import React, { useState } from "react";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
-import { Box, Typography, Toolbar, AppBar } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Toolbar,
+  AppBar,
+  Container,
+  IconButton,
+  Button,
+  Tooltip,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  Drawer,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useTheme } from "next-themes";
 import ThemeUpdater from "./ThemeUpdater";
 
+const pages = ["Home", "About", "Contact"];
+const drawerWidth = 240;
 interface Props {
   window?: () => Window;
   children: React.ReactElement;
@@ -25,9 +44,36 @@ function ElevationScroll(props: Props) {
     elevation: trigger ? 4 : 0,
   });
 }
-
-const Header = () => {
+interface HeaderProps {
+  window?: () => Window;
+}
+const Header = (props: HeaderProps) => {
   const { resolvedTheme } = useTheme();
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        FEYZO.TECH
+      </Typography>
+      <Divider />
+      <List>
+        {pages.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
   return (
     <>
       <ElevationScroll>
@@ -35,14 +81,109 @@ const Header = () => {
           color="default"
           sx={{ bgcolor: resolvedTheme === "light" ? "white" : "" }}
         >
-          <Toolbar>
-            <Box sx={{ flexGrow: 1 }}>
-              <Typography variant="h6" component="div" sx={{ mr: 2 }}>
-                Scroll to elevate App bar
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                FEYZO.TECH
               </Typography>
-            </Box>
-            <ThemeUpdater />
-          </Toolbar>
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleDrawerToggle}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+              </Box>
+              <Box component="nav">
+                <Drawer
+                  container={container}
+                  variant="temporary"
+                  open={mobileOpen}
+                  onClose={handleDrawerToggle}
+                  ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                  }}
+                  sx={{
+                    display: { xs: "block", sm: "none" },
+                    "& .MuiDrawer-paper": {
+                      boxSizing: "border-box",
+                      width: drawerWidth,
+                    },
+                  }}
+                >
+                  {drawer}
+                </Drawer>
+              </Box>
+
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  marginLeft: 2,
+                  display: { xs: "none", md: "flex" },
+                }}
+              >
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    sx={{ my: 2, color: "inherit", display: "block" }}
+                  >
+                    <Typography
+                      sx={{
+                        fontFamily: "monospace",
+                        fontWeight: 600,
+                        letterSpacing: ".1rem",
+                      }}
+                    >
+                      {page}
+                    </Typography>
+                  </Button>
+                ))}
+              </Box>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Update Theme">
+                  <ThemeUpdater />
+                </Tooltip>
+              </Box>
+            </Toolbar>
+          </Container>
         </AppBar>
       </ElevationScroll>
       <Toolbar />
